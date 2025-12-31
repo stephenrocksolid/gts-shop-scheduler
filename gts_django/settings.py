98bf630d7643a824c58ae7ae1b5194f6448f4de3
joshuaelitesoftware <joshua.elitesoftware@gmail.com>
2025-12-30 21:04:02 -0600

    Update DEBUG setting in settings.py to use environment variable
    - Changed the DEBUG setting to read from an environment variable, allowing for more flexible configuration between development and production environments.
    - This enhances security by preventing accidental exposure of sensitive information in production.

--- gts_django/settings.py
+++ gts_django/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-development-key-change-in-production-12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("1", "true", "yes", "on")
 
 ALLOWED_HOSTS = ['*']
 
